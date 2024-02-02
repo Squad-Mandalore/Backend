c0028dc28512de13d93dc94b6ef12b53a833cee5
BB223 <79644542+BB223@users.noreply.github.com>
2024-02-03 00:33:16 +0100

    chore: SM-105: refactor existing code to use new model architecture

--- src/database/database_setup.py
+++ src/database/database_setup.py
@@ -1,22 +1,15 @@
-from typing import TypeVar
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+from sqlalchemy.orm import sessionmaker
+from src.models.base_model import Base
 
 # mashallah in memory engine bro
-engine = create_engine('sqlite:///./test.db')
+engine = create_engine('sqlite:///../../db/test.db', connect_args={"check_same_thread": False})
 
 # with sqlalchemy you are able to store python objects in a database,
 # therefore you first need to create some python classes
 
-# Base class to inherit from
-Base = declarative_base()
-DBModel = TypeVar('DBModel', bound=Base)
-
 # Set up the session
-Session = sessionmaker(bind=engine)
-session = Session()
-
+SessionLocal = sessionmaker(bind=engine)
 
 # create db shiat
 def init_db() -> None:
--- src/main.py
+++ src/main.py
@@ -2,10 +2,9 @@ from .database.database_setup import init_db
 from fastapi import FastAPI
 from .controllers import password_controller, user_controller
 
+init_db()
 
 app = FastAPI()
-
-init_db()
 
 app.include_router(password_controller.router)
 app.include_router(user_controller.router)

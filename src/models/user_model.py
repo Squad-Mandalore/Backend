c9ac3b37510ff1559bb2f622ebd336107f2c81d9
BB223 <79644542+BB223@users.noreply.github.com>
2024-02-03 00:31:13 +0100

    ci: SM-105: add base_model and user_model

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,26 +1,57 @@
-from sqlalchemy import Column, String
-from src.database.database_setup import Base
-from src.database.database_utils import get_uuid
+from datetime import date, datetime
+import enum
+import uuid
 
+from sqlalchemy import Enum, ForeignKey, Uuid
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.models.base_model import Base
+
+class Gender(enum.Enum):
+    MALE = 'm'
+    FEMALE = 'f'
+    DIVERSE = 'd'
 
 class User(Base):
-    __tablename__ = 'user'
-    id = Column("id", String, primary_key=True)
-    # TODO combine id and username as primery key
-    username = Column("username", String)
-    password = Column("password", String)
-    salt = Column("salt", String)
-    lastname = Column("lastname", String)
-    forename = Column("forename", String)
-    created_at = Column("created_at", String)
-    last_edited_at = Column("last_edited_at", String)
-    last_password_change = Column("last_password_change", String)
-    email = Column("email", String)
+    __tablename__ = "user"
+    id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
+    username: Mapped[str]= mapped_column(unique=True)
+    email: Mapped[str]
+    hashed_password: Mapped[str]
+    firstname: Mapped[str]
+    lastname: Mapped[str]
+    created_at: Mapped[datetime]
+    last_password_change: Mapped[datetime]
+    last_edited_at: Mapped[datetime]
+    type: Mapped[str]
 
-    def __init__(self, password: str, salt: str):
-        self.id = get_uuid()
-        self.password = password
-        self.salt = salt
+    __mapper_args__ = { "polymorphic_on": "type", "polymorphic_abstract": True}
 
-    def __repr__(self):
-        return f"{self.password}, {self.salt}"
+class Administrator(User):
+    __tablename__ = "administrator"
+    uses_otp: Mapped[bool]
+    id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"), primary_key=True)
+
+    __mapper_args__ = { "polymorphic_identity": "administrator" }
+
+class Trainer(User):
+    __tablename__ = "trainer"
+    birthday: Mapped[date] = mapped_column(nullable=True)
+    uses_otp: Mapped[bool]
+    id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"), primary_key=True)
+
+    athletes: Mapped[list["Athlete"]] = relationship(back_populates="trainer")
+
+    __mapper_args__ = { "polymorphic_identity": "trainer" }
+
+class Athlete(User):
+    __tablename__ = "athlete"
+    birthday: Mapped[date]
+    gender: Mapped[Gender] = mapped_column(Enum(Gender), default=Gender.DIVERSE)
+    has_disease: Mapped[bool] = mapped_column(default=False)
+    trainer_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("trainer.id"))
+    id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"), primary_key=True)
+
+    trainer: Mapped["Trainer"] = relationship(back_populates="athletes", primaryjoin="Trainer.id==Athlete.trainer_id")
+
+    __mapper_args__ = { "polymorphic_identity": "athlete" }

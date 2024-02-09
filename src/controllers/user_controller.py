089776babb537cd5e2d7f2f1af0ca54ff1a5ef06
mitsaucepls <spongebobmarkus@gmail.com>
2024-02-09 11:29:58 +0100

    ci: SM-70 merge SM-89 and resolve conflicts

--- src/controllers/user_controller.py
+++ src/controllers/user_controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from src.models.models import Base, User, Trainer, Administrator, Athlete, Code
+from src.models.models import Base, User
 from src.database.database_utils import get_all, get_db
 from src.schemas.password_schema import PasswordSchema
 from src.schemas.user_schema import UserSchema
@@ -27,22 +27,22 @@ async def get_all_entries(db: Session = Depends(get_db)) -> list[Base]:
 
 
 @router.post("/signup", response_model=UserSchema, status_code=status.HTTP_201_CREATED)
-async def post_password(password_schema: PasswordSchema) -> User:
+async def post_password(password_schema: PasswordSchema, db: Session = Depends(get_db)) -> User:
     password = password_schema.password
     validated = validate_password(password)
     if isinstance(validated, HTTPException):
         raise validated
 
     hashed_password, salt = hash_and_spice_password(password)
-    user = add_user_with_pw(hashed_password, salt)
+    user = add_user_with_pw(db, hashed_password, salt)
     return user
 
 
 @router.post("/login", status_code=status.HTTP_202_ACCEPTED) # will probably replaced with verify user and then 2FA and jwt token
-async def post_login(user_schema: UserSchema) -> None:
+async def post_login(user_schema: UserSchema, db: Session = Depends(get_db)) -> None:
     id = user_schema.id
     password = user_schema.password
 
-    checked = check_password(id, password)
+    checked = check_password(db, id, password)
     if isinstance(checked, HTTPException):
         raise checked
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,14 +1,15 @@
 from typing import Optional
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
 from src.database.database_utils import add, get_by_id
 from src.models.models import User
 from src.services.password_service import hash_and_spice_password
 
 
-def check_password(id: str, password: str) -> Optional[HTTPException]:
+def check_password(db: Session, id: str, password: str) -> Optional[HTTPException]:
     # get the user from the database
-    user = get_by_id(User, id)
+    user = get_by_id(db, User, id)
     if isinstance(user, HTTPException):
         return user
 
@@ -18,7 +19,7 @@ def check_password(id: str, password: str) -> Optional[HTTPException]:
         return HTTPException(status_code=401, detail="Password is incorrect")
 
 
-def add_user_with_pw(hashed_password: str, salt: str) -> User:
-    user = User(hashed_password, salt)
-    add(user)
+def add_user_with_pw(db: Session, hashed_password: str, salt: str) -> User:
+    user = User(hashed_password=hashed_password, salt=salt, username="Gopher", email="", firstname="", lastname="")
+    add(db, user)
     return user

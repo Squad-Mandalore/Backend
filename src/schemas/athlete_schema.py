13222af115560174d699edd27b846ca5b0917864
ole361 <grundmann181@gmail.com>
2024-02-15 20:20:01 +0100

    feat: SM-70 added integration tests instead of unit tests

--- src/schemas/athlete_schema.py
+++ src/schemas/athlete_schema.py
@@ -1,7 +1,19 @@
+from datetime import date
+
 from pydantic import BaseModel
+
+from src.models.models import Gender, Trainer
 
 
 class AthleteSchema(BaseModel):
     # model_config = ConfigDict()
-    id: str
-    password: str
+    username: str
+    email: str
+    hashed_password: str
+    firstname: str
+    lastname: str
+    salt: str
+    birthday: date
+    gender: Gender
+    has_disease: bool
+    trainer: Trainer

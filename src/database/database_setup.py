992041a75acef765cfb53811a7112e4861602948
Markus Siegert <spongemarkus@gmail.com>
2024-03-16 01:00:59 +0100

    ci: parsing values.json into database objects on every database creation

--- src/database/database_setup.py
+++ src/database/database_setup.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from src.models.models import Base
+from src.models.values import parse_values
+from src.logger.logger import logger
 
-# mashallah in memory engine bro
-engine = create_engine('sqlite:///db/test.db', connect_args={"check_same_thread": False})
+db_path: str = '/db/test.db'
+engine = create_engine(f'sqlite://{db_path}', connect_args={"check_same_thread": False})
 
 # with sqlalchemy you are able to store python objects in a database,
 # therefore you first need to create some python classes
@@ -14,4 +17,7 @@ SessionLocal = sessionmaker(bind=engine)
 
 # create db shiat
 def init_db() -> None:
-    Base.metadata.create_all(bind=engine)
+    if not os.path.exists('.' + db_path):
+        logger.info("Creating database")
+        Base.metadata.create_all(bind=engine)
+        parse_values(SessionLocal())

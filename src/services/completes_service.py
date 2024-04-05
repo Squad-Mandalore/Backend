52bf15c183a796e476b003039c233123bbbf3778
BB223 <79644542+BB223@users.noreply.github.com>
2024-04-05 18:32:40 +0200

    ci: add point calc

--- src/services/completes_service.py
+++ src/services/completes_service.py
@@ -1,10 +1,13 @@
+from datetime import date
 from typing import cast
 
 from fastapi import HTTPException, status
+from sqlalchemy import false, select
 from sqlalchemy.orm import Session
+from starlette.status import HTTP_404_NOT_FOUND
 
 from src.database import database_utils
-from src.models.models import Base, Completes
+from src.models.models import Athlete, Base, Completes, Rule
 from src.schemas.completes_schema import CompletesPatchSchema, CompletesPostSchema
 from src.services import update_service
 
@@ -37,3 +40,39 @@ def delete_completes(id: str, db: Session) -> None:
 
 def get_all_completes(db: Session) -> list[Completes]:
     return cast(list[Completes], database_utils.get_all(Completes, db))
+
+def calculate_points(completes: Completes,athlete: Athlete, db: Session):
+    tracket_at = completes.tracked_at
+    exercise_id = completes.exercise_id
+    result = completes.result
+    isbigger = True
+    athlete_age = tracket_at.year - athlete.birthday.year
+
+    rule: Rule | None = db.scalar(select(Rule).where(Rule.exercise_id == exercise_id,
+                                             Rule.gender == athlete.gender,
+                                             Rule.from_age <= athlete_age,
+                                             Rule.to_age >= athlete_age))
+
+    if(rule == None):
+        return 0
+
+    if(rule.bronze > rule.gold):
+        isbigger = False
+
+    points = 0
+    if isbigger:
+        if result >= rule.gold:
+            points = 3
+        elif result >= rule.silver:
+            points = 2
+        elif result >= rule.bronze:
+            points = 1
+    else:
+        if result <= rule.gold:
+            points = 3
+        elif result <= rule.silver:
+            points = 2
+        elif result <= rule.bronze:
+            points = 1
+
+    return points

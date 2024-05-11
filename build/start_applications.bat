464a46b62c871c36def5d6ef81416d3a5f38b7bf
Markus Siegert <markus.siegert@telekom.de>
2024-05-11 13:28:59 +0200

    ci: SM-60 installation scripts for windows and linux

---
+++ build/start_applications.bat
@@ -0,0 +1,8 @@
+@echo off
+
+docker load -i backend_image.tar
+docker load -i frontend_image.tar
+
+docker-compose up -d
+
+pause
---
+++ build/start_applications.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker load -i ./backend_image.tar
+docker load -i ./frontend_image.tar
+
+docker-compose up -d

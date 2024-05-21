083989c6e72e658b65851ad048bddb5f094ccd1f
Markus Siegert <markus.siegert@telekom.de>
2024-05-21 21:05:32 +0200

    fix: SM-999 everything

--- start_applications.bat
+++ start_applications.bat
@@ -3,6 +3,6 @@
 docker load -i path_to_your_backend_image_tar\backend_image.tar
 docker load -i path_to_your_frontend_image_tar\frontend_image.tar
 
-docker-compose up -d
+docker compose up -d
 
 pause
